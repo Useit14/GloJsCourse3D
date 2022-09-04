81be9d1568573a1806c7cdbd384ca3e9c1f995a2
rjarovoj@web-bee.ru <useit14@inbox.ru>
2022-09-04 13:33:50 +0300

    Start project

---
+++ src/modules/validationForm.js
@@ -0,0 +1,49 @@
+const validation = () => {
+  const form1 = document.getElementById("form1");
+
+  const validate = (form) => {
+    let isError = false;
+    const inputsText = form.querySelectorAll("input[type=text]");
+    const inputsEmail = form.querySelectorAll(".form-email");
+    const inputsTel = form.querySelectorAll(".form-phone");
+
+    inputsText.forEach((input) => {
+      input.addEventListener("change", (e) => {
+        if (/\d+/gi.test(e.target.value) && e.target.value !== "")
+          isError && alert("Ошибка: Цифры не допустимы");
+        e.target.value = "";
+      });
+    });
+
+    inputsEmail.forEach((input) => {
+      input.addEventListener("change", (e) => {
+        if (
+          /[^[a-z\-\_\.\!\~\*]+@[a-z\-\_\!\~\*]+[\.][a-z\-\_\!\~\*]+]/gi.test(
+            e.target.value
+          ) &&
+          e.target.value !== ""
+        ) {
+          alert("Ошибка: Неккоретный почтовый ящик");
+          e.target.value = "";
+        }
+      });
+    });
+
+    inputsTel.forEach((input) => {
+      input.addEventListener("change", (e) => {
+        if (/[^\d-\(\)]+/gi.test(e.target.value) && e.target.value !== "") {
+          alert("Ошибка: Неккоретный номер телефона");
+          e.target.value = "";
+        }
+      });
+    });
+  };
+
+  form1.addEventListener("submit", (form1) => {
+    event.preventDefault();
+    validate(form1);
+  });
+  validate(form1);
+};
+
+export default validation;
