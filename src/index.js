1d1d7476d9960c6a7c4f84835615e7a692f60c33
rjarovoj@web-bee.ru <useit14@inbox.ru>
2022-09-04 23:11:28 +0300

    Mission is complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validate from "./modules/validationForm";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
 timer("31 august 2022");
 menu();
 modal();
 validate();
 tabs();
+slider([
+  "portfolio-content",
+  "portfolio-item",
+  "dot",
+  "portfolio-item-active",
+  "dot-active",
+]);
