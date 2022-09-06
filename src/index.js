7351288fb87cf96ca54d497f6975e1660c4ef28b
rjarovoj@web-bee.ru <useit14@inbox.ru>
2022-09-06 18:12:08 +0300

    Mission is complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validateForm from "./modules/validationForm";
 import validateCalc from "./modules/validationCalc";
+import calculate from "./modules/calc";
 
 timer("31 august 2022");
 menu();
 modal();
 validateForm();
 validateCalc();
+calculate(100);
---
+++ src/modules/calc.js
@@ -0,0 +1,59 @@
+/* eslint-disable indent */
+const calc = (price = 100) => {
+  const calcBlock = document.querySelector(".calc-block");
+  const calcType = document.querySelector(".calc-type");
+  const calcSquare = document.querySelector(".calc-square");
+  const calcCount = document.querySelector(".calc-count");
+  const calcDay = document.querySelector(".calc-day");
+  const total = document.getElementById("total");
+  let interval;
+
+  const animateNumber = (end) => {
+    if (+total.textContent !== end) {
+      if (+total.textContent < end) {
+        total.textContent = +total.textContent + 1;
+      } else {
+        total.textContent = +total.textContent - 1;
+      }
+    } else {
+      clearInterval(interval);
+    }
+  };
+
+  const countCalc = () => {
+    const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+    const calcSquareValue = calcSquare.value;
+    let totalValue = 0;
+    let calcCountValue = 1;
+    let calcDayValue = 1;
+
+    if (calcCount.value > 1) {
+      calcCountValue += +calcCount.value / 10;
+    }
+    if (calcDay.value && calcDay.value < 5) {
+      calcDayValue = 2;
+    } else if (calcDay.value && calcDay.value < 10) {
+      calcDayValue = 1.5;
+    }
+    if (price * calcTypeValue * calcSquareValue) {
+      totalValue =
+        price * calcTypeValue * calcSquareValue * calcCountValue * calcDayValue;
+    } else {
+      totalValue = 0;
+    }
+    interval = setInterval(animateNumber, 1, totalValue);
+  };
+
+  calcBlock.addEventListener("input", (e) => {
+    if (
+      e.target === calcType ||
+      e.target === calcSquare ||
+      e.target === calcCount ||
+      e.target === calcDay
+    ) {
+      countCalc();
+    }
+  });
+};
+
+export default calc;
